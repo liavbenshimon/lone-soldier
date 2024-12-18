49f200fec70ce7f2654fa857fa42d47fd0fc322a
liav <liav1232111@gmail.com>
2024-12-18 17:41:49 +0200

    fix signup

--- frontend-app/src/components/login-form.tsx
+++ frontend-app/src/components/login-form.tsx
@@ -59,7 +59,7 @@ export function LoginForm({
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
               <button
-                onClick={() => navigate("/singup")}
+                onClick={() => navigate("/signup")}
                 className="underline underline-offset-4"
               >
                 Sign up
