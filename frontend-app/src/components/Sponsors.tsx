feba494d39d0e8a2e23097afab7543bdc4f3f79c
liav <liav1232111@gmail.com>
2024-12-19 08:26:01 +0200

    Supporters

--- frontend-app/src/components/Sponsors.tsx
+++ frontend-app/src/components/Sponsors.tsx
@@ -36,7 +36,7 @@ export const Sponsors = () => {
   return (
     <section id="sponsors" className="container pt-24 sm:py-32">
       <h2 className="text-center text-md lg:text-xl font-bold mb-8 text-primary">
-        Institution
+        Supporters
       </h2>
 
       <div className="flex flex-wrap justify-center items-center gap-4 md:gap-8">
