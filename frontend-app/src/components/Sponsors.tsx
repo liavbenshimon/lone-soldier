d385439725932ba2df48dc0822d06f52d17fa0aa
liav <liav1232111@gmail.com>
2024-12-19 08:32:34 +0200

    xl

--- frontend-app/src/components/Sponsors.tsx
+++ frontend-app/src/components/Sponsors.tsx
@@ -35,7 +35,7 @@ const sponsors: SponsorProps[] = [
 export const Sponsors = () => {
   return (
     <section id="sponsors" className="container pt-24 sm:py-32">
-      <h2 className="text-center text-md lg:text-xl font-bold mb-8 text-primary">
+      <h2 className="text-center text-5xl lg:text-xl font-bold mb-8 text-primary">
         Supporters
       </h2>
 
