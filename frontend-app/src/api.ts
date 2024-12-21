7153c1f47a9ce43ca83b35525a97f4bf1f142bdb
shalev396 <shalev396@gmail.com>
2024-12-21 21:24:45 +0200

    changed the server ip to the new one

--- frontend-app/src/api.ts
+++ frontend-app/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://85.250.92.38:5000",
+  baseURL: "https://lone-soldier.onrender.com",
   headers: {
     Authorization: `Barener ${sessionStorage.getItem("token")}`,
     "Access-Control-Allow-Methods": "true",
